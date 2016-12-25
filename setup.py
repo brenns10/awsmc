c85994ccdac7a3a95786a9bf8c09ff5f1d086129
Stephen Brennan <smb196@case.edu>
2016-12-25 17:34:25 -0500

    Packaging

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='awsmc',
+    version='0.0.1',
+    description='Simple and cheap minecraft hosting on AWS',
+    long_description=long_description,
+    install_requires=['paramiko', 'requests', 'screenutils', 'boto3'],
+    url='https://github.com/brenns10/awsmc',
+    author='Stephen Brennan',
+    author_email='stephen@brennan.io',
+    license='Revised BSD',
+    py_modules=['awsmc'],
+    entry_points={
+        'console_scripts': [
+            'awsmc = awsmc.__main__.main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 4 - Beta',
+    ],
+    keywords='AWS minecraft',
+)
